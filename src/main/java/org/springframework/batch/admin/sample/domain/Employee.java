a6e211b3335686d7044d39baa8dad124f85277c5
ericfoster <eric_foster73@hotmail.com>
2017-05-23 11:36:03 -0400

    push of codebase

---
+++ src/main/java/org/springframework/batch/admin/sample/domain/Employee.java
@@ -0,0 +1,40 @@
+package org.springframework.batch.admin.sample.domain;
+
+public class Employee {
+    private String name;
+    private String role;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
